a544ded0830909a7e692468f8446da5b82e5b861
sshleifer <sam@kensho.com>
2016-09-28 18:13:25 -0700

    SGD can take a loss function  as an argument

--- code/gradient_descent.py
+++ code/gradient_descent.py
@@ -4,14 +4,14 @@ import numpy as np
 import pandas as pd
 from collections import defaultdict
 
-from code.helpers import f_gauss, d_gauss, f_bowl, d_bowl, get_lr
+from code.helpers import f_gauss, d_gauss, f_bowl, d_bowl, get_lr, j#, j2
 from code.constants import *
+X1 = pl.loadtxt('hw1code/P1/fittingdatap1_x.txt')
+Y1 = pl.loadtxt('hw1code/P1/fittingdatap1_y.txt')
 
-X = pl.loadtxt('hw1code/P1/fittingdatap1_x.txt')
-y = pl.loadtxt('hw1code/P1/fittingdatap1_y.txt')
 
 def _gradient_descent(func, deriv_func=None,
-                      init_weights=np.array([5.,5.]), lr=1, stop_crit=1e-6,
+                      init_weights=np.array([5., 5.]), lr=1, stop_crit=1e-6,
                       h=1e-3, max_iter=1000):
     '''Generic gradient descent function
     Args:
@@ -53,7 +53,8 @@ def gradient_descent(*args, **kwargs):
 def numerical_gradient(x, f, h=0.00001):
     '''Numerically evaluate the gradient of f at x using central differences'''
     out = np.zeros(len(x))
-    assert x.dtype == float
+    if x.dtype != float:
+        x = x.astype(float)
     assert not np.isnan(x).any()
     assert h > 0
     hfix = 2 * h
@@ -62,12 +63,13 @@ def numerical_gradient(x, f, h=0.00001):
         hminus = np.copy(x)
         hplus[i] = hplus[i] + h
         hminus[i] = hminus[i] - h
+        assert hminus[i] != x[i]
         out[i] = (f(hplus) - f(hminus)) / hfix
         assert not np.isnan(out[i]), 'out:{}, x:{}'.format(out, x)
     return out
 
 
-def SGD(init_weights=np.zeros(X.shape[1]), stop_crit=1e-3, h=1e-3, max_iter=10000, lr=1e-5):
+def SGD(loss_fn=j, init_weights=np.zeros(10), stop_crit=1e-3, h=1e-3, max_iter=10000, lr=1e-5):
     '''Generic gradient descent function
     Args:
 
@@ -77,13 +79,13 @@ def SGD(init_weights=np.zeros(X.shape[1]), stop_crit=1e-3, h=1e-3, max_iter=1000
         h: step size for computing numerical gradient
         max_iter: how many iterations before stopping
     '''
-    count = 0; n = 0; f_call = 0;
+    count = 0; n = 0
     cur_weights = np.copy(init_weights)
     paths = defaultdict(list)
     for n in range(max_iter):
-        i = np.random.randint(0, len(X))
+        i = np.random.randint(0, len(X1))
         cur_lr = get_lr(n) * lr
-        func = funcy.partial(j, i)
+        func = funcy.partial(loss_fn, i)
         local_value = func(cur_weights)
         gradient = numerical_gradient(cur_weights, func, h)
         cur_weights = cur_weights - cur_lr * gradient
@@ -96,7 +98,7 @@ def SGD(init_weights=np.zeros(X.shape[1]), stop_crit=1e-3, h=1e-3, max_iter=1000
         paths['w0'].append(cur_weights[0])
         paths['lr'].append(get_lr(n))
         count = count + 1 if delta < stop_crit else 0
-        #if count >= 3:break
+        if count >= 3:break
     print 'done in {} steps'.format(n)
     return cur_weights, pd.DataFrame(paths)
 
@@ -117,4 +119,5 @@ if __name__ == '__main__':
     STEP_SIZES = [1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7]
     gauss_errors = pd.Series({h: g_error(9.995, h=h) for h in STEP_SIZES})
     bowl_errors = pd.Series({h: b_error(9.995, h=h) for h in STEP_SIZES})
+    SGD()
     print gauss_errors, bowl_errors
