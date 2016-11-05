5a43bd9a112334f64dc850f54287d992696e9ecc
sshleifer <sam@kensho.com>
2016-11-05 03:00:58 -0400

    feed forward working

--- HW3/code/nn.py
+++ HW3/code/nn.py
@@ -1,51 +1,70 @@
 import numpy as np
+# from sklearn.preprocessing import OneHotEncoder
+
 
 def softmax(x):
     return np.max(x, 0)
 
+def dsigmoid(y): return 1.0 - y*y
+
+# TODO(SS): Cross-entropy loss
+
+
 class NN(object):
-    
-    def __init__(self, n_hidden_layers=2, n_hidden_nodes=2, activation_func=softmax, epochs=10):
-        self.z = np.zeros((n_hidden_layers + 2, n_hidden_nodes))
-        self.a = np.zeros((n_hidden_layers + 2, n_hidden_nodes))
-        self.w = np.ones((n_hidden_layers + 2, n_hidden_nodes))
-        self.n_layers =  n_hidden_layers + 2
+    '''Neural Network Implementation'''
+
+    def __init__(self, X, y, nh=2, no=4, activation_func=softmax, epochs=10):
+        '''Neural Network Implementation
+        Args:
+            nh: # hidden layers
+            n_inputs: # nodes per layer
+            no: # outputs
+        '''
+        n_inputs = X.shape[1]
+        self.z = np.zeros((nh + 2, n_inputs))
+        self.a = np.zeros((nh + 2, n_inputs))
+        self.w = np.ones((nh + 2, n_inputs))
+        # self.one_hot_y = OneHotEncoder(sparse=False).fit_transform(y)
+        self.one_hot_y = np.array([[1 if yval == j else 0 for j in np.unique(y)] for yval in y])
+
+        # Output shit
+        #self.output = np.zeros(no)
+        self.wo = np.ones((n_inputs, no))
+        self.zo = np.zeros(no)
+
+        self.n_layers = nh + 2
         self.activate = activation_func
         self.epochs = epochs
-        self.bias = np.zeros(n_hidden_layers)
-        self.n_hidden_layers = n_hidden_layers
-        self.n_hidden_nodes = n_hidden_nodes
-        self.d = np.array((n_hidden_layers, n_hidden_nodes, n_hidden_layers)) # matrix of errors for each layer
+        self.bias = np.zeros(nh)
+        self.nh = nh
+        self.n_inputs = n_inputs
+        self.d = np.array((nh, n_inputs, nh))  # matrix of errors for each layer
 
+    @staticmethod
+    def final_activate(z):
+        return np.exp(z) / np.sum(np.exp(z))
 
     def predict(self, x=None):
         '''pass one x row through the network'''
-        x=X[2]
         self.a[0] = x
-        for layer in range(1, self.n_hidden_layers+1):
-            #print layer#, self.a, self.z
-            #print self.a[layer]
-            self.z[layer] = self.w[layer].T.dot(self.a[layer - 1]) # aggregate
+        for layer in range(1, self.nh + 2):
+            self.z[layer] = self.w[layer].T.dot(self.a[layer - 1])  # aggregate
             self.a[layer] = self.activate(self.z[layer])
-                
-        # final layer
-        return self.activate(self.a[layer])
-    
-    #def calc_error(self, x, weight):
-        
-    
+        self.zo = self.wo.T.dot(self.a[layer])
+        return self.final_activate(self.zo)
+
     def fit(self, X, y):
+        '''stochastically'''
         for epoch in range(self.epochs):
             i = np.random.randint(0, len(X))
-            x, target = X[i], y[i]
-            prediction =  self.predict(x)
-            self.backprop(target - error)
-            
+            x, target = X[i], self.one_hot_y[i]
+            predicted_probas = self.predict(x)
+            loss = -np.sum(target * np.log(predicted_probas))
+            self.backprop(loss)
+
     def backprop(self, error):
-        
-        return
+        :0
+        raise NotImplementedError
         # pseudocode
-        
-    def error_mat(self):
-        #np.diag()
-        return
+
+
