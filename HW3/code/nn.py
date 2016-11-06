9e6daa8c4d981438af8e125f09248f50adb58d88
sshleifer <sam@kensho.com>
2016-11-06 16:07:16 -0500

    Backprob fixes: updates same shape as w[layer]

--- HW3/code/nn.py
+++ HW3/code/nn.py
@@ -3,11 +3,11 @@ from sklearn.preprocessing import OneHotEncoder
 # from scipy.misc import derivative
 
 
-def softmax(x):
-    return np.max(x, 0)
+def relu(x):
+    return np.clip(x, 0, None)
 
 def dsigmoid(y):
-    return 1.0 - y*y
+    return 1.0 - y * y
 
 
 def dsoftmax(x):
@@ -30,7 +30,7 @@ def stable_softmax(x):
 class NN(object):
     '''Neural Network Implementation'''
 
-    def __init__(self, X, y, nh=2, no=4, activation_func=softmax, epochs=10):
+    def __init__(self, X, y, nh=2, hidden_nodes=2, activation_func=relu, epochs=100):
         '''Neural Network Implementation
         Args:
             nh: # hidden layers
@@ -38,60 +38,95 @@ class NN(object):
             no: # outputs
         '''
         n_inputs = X.shape[1]
-        self.z = np.zeros((nh + 2, n_inputs))
-        self.a = np.zeros((nh + 2, n_inputs))
-        self.w = np.ones((nh + 2, n_inputs))
-        self.sk_one_hot_y = OneHotEncoder(sparse=False).fit_transform(y)
+        self.epochs = epochs
+        self.nh = nh
+        self.L = nh + 2
+        self.n_inputs = n_inputs
+        self.n_hidden_nodes = hidden_nodes
+        self.n_outputs = len(np.unique(y))
+        self.z = np.zeros((self.L, hidden_nodes))
+        self.b = np.zeros((self.L, hidden_nodes))
+        self.a = np.zeros((self.L, hidden_nodes))
+        self.w = {}#np.ones((self.L, hidden_nodes))
         self.one_hot_y = np.array([[1 if yval == j else 0 for j in np.unique(y)] for yval in y])
 
         # Output shit
         #self.output = np.zeros(no)
-        self.wo = np.ones((n_inputs, no))
-        self.zo = np.zeros(no)
+        self.wo = np.ones((hidden_nodes, self.n_outputs))
+        self.bo = np.zeros(self.n_outputs)
+        self.zo = np.zeros(self.n_outputs)
 
         self.n_layers = nh + 2
         self.activate = activation_func
-        self.epochs = epochs
         self.bias = np.zeros(nh)
-        self.nh = nh
-        self.n_inputs = n_inputs
         self.d = np.array((nh, n_inputs, nh))  # matrix of errors for each layer
 
     @staticmethod
     def final_activate(z):
         return np.exp(z) / np.sum(np.exp(z))
 
-    def _predict_row(self, x):
+    def feedforward(self, x):
         '''pass one x row through the network'''
         self.a[0] = x
-        for layer in range(1, self.nh + 2):
-            self.z[layer] = self.w[layer].T.dot(self.a[layer - 1])  # aggregate
+        for layer in range(1, self.L):
+            if layer not in self.w:
+                self.w[layer] = np.ones((len(self.a[layer-1]), len(self.z[layer])))
+            assert self.w[layer].shape == (self.n_hidden_nodes, self.n_hidden_nodes)
+            self.z[layer] = self.w[layer].T.dot(self.a[layer - 1]) + self.b[layer]# aggregate
             self.a[layer] = self.activate(self.z[layer])
-        self.zo = self.wo.T.dot(self.a[layer])
+        self.zo = self.wo.T.dot(self.a[layer]) + self.bo
         self.ao = self.final_activate(self.zo)
         return self.ao
 
     def predict_probas(self, X):
-        return np.apply_along_axis(self._predict_row, 1, X)
+        return np.apply_along_axis(self.feedforward, 1, X)
 
     def fit(self, X, y):
         '''stochastically'''
         for epoch in range(self.epochs):
             i = np.random.randint(0, len(X))
             x, target = X[i], self.one_hot_y[i]
-            predicted_probas = self._predict_row(x)
+            print x, target
+            predicted_probas = self.feedforward(x)
             loss = target - predicted_probas
             self.backprop(loss)
+            #cross_entropy_loss = -np.sum(np.log(predicted_probas) * target)
+            #print cross_entropy_loss
+            # self.backprop(cross_entropy_loss)
+
+    def score(self, X, y):
+        predicted_probas = self.predict_probas(X)
+        return -np.sum(np.log(predicted_probas) - y)
+
+    def _updates(self, last_a, delta):
+        a_reshape = last_a.reshape(last_a.shape[0], 1)
+        err_reshape = delta.reshape(delta.shape[0], 1).T
+        return a_reshape.dot(err_reshape)
 
     def backprop(self, error, lr=1.):
         '''Propagate error back through net work and update weights'''
-        #delta = np.zeros((self.nh + 2, self.n_inputs))
-        # import ipdb; ipdb.set_trace()
+        # delta[-1] = np.diag(map(dsoftmax, self.zo)).dot(dsigmoid(self.ao) * error)
+        self.bo -= error * lr
+        #a_reshape = self.a[self.L - 1].reshape(self.wo.shape[0], 1)
+        #err_reshape = error.reshape(self.wo.shape[1], 1).T
+        updates = self._updates(self.a[self.L - 1], error)
+        # a_reshape.dot(err_reshape)
+        assert updates.shape == self.wo.shape
+        self.wo = self.wo - updates * lr
+        print self.wo
         deltas = [error]
-        #delta[-1] = np.diag(map(dsoftmax, self.zo)).dot(dsigmoid(self.ao) * error)
-        self.wo += (deltas[-1] * self.ao) * lr
-        for layer in range(self.nh + 1, 1):
+        for layer in reversed(range(1, self.L)):
             last_w = self.w[layer + 1] if layer < self.nh + 1 else self.wo
             new_delta = np.diag(map(dsoftmax, self.z[layer])).dot(last_w).dot(deltas[-1])
+            self.b[layer] = self.b[layer] - new_delta
+
+            # above is 2 element array
+            assert new_delta.shape == (self.n_hidden_nodes,)
+            updates = self._updates(self.a[layer - 1], new_delta) * lr
+            print updates
+            #updates = self.a[layer - 1].dot(new_delta.T) * lr# should be of shape W
+            assert updates.shape == self.w[layer].shape
+            self.w[layer] = self.w[layer] - updates
+            assert not np.isnan(self.w[layer]).any()
             deltas.append(new_delta)
-            self.w[layer] += lr * self.a[layer + 1].dot(new_delta.T)
+        return deltas
--- HW3/code/test_nn.py
+++ HW3/code/test_nn.py
@@ -8,17 +8,18 @@ df = pd.read_csv('hw3_resources/data/data_3class.csv', header=None, sep=' ')
 X = df[[0, 1]].as_matrix()
 y = df[2].as_matrix()
 n_classes = len(np.unique(y))
-nn = NN(X, y, epochs=2, no=n_classes)
+nn = NN(X, y, epochs=3)
 
 
 class TestNN(unittest.TestCase):
 
     def test_predict(self):
-        output = nn._predict_row(X[1])
+        output = nn.feedforward(X[1])
         self.assertEqual(len(output), len(np.unique(y)))
 
     def test_backprop(self):
         nn.fit(X, y)
+        self.assertGreater(np.max(nn.w.values()), 1., 'weights didnt update happen')
         predicted_probas = nn.predict_probas(X)
-        import ipdb; ipdb.set_trace()
         self.assertEqual(predicted_probas.shape, nn.one_hot_y.shape)
+        # import ipdb; ipdb.set_trace()
