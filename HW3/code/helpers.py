648f2449130b6238961016984dc10d3c1db8af60
sshleifer <sam@kensho.com>
2016-11-06 22:06:43 -0500

    helpers factored out

---
+++ HW3/code/helpers.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def relu(x):
+    return np.clip(x, 0, None)
+
+def dsigmoid(y):
+    return 1.0 - y * y
+
+
+def dsoftmax(x):
+    return np.clip(np.sign(x), 0, 1)
+
+
+def stable_softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+
+def assert_no_nans(struct):
+    '''Make sure there are no np.nan floating around in the array/dict'''
+    if isinstance(struct, dict):
+        ret_val = np.isnan(struct.values()).any()
+    else:
+        ret_val = np.isnan(struct).any()
+    if ret_val:
+        raise AssertionError(struct)
--- HW3/code/nn.py
+++ HW3/code/nn.py
@@ -1,30 +1,6 @@
 import numpy as np
+from code.helpers import relu, dsoftmax, stable_softmax, assert_no_nans
 np.random.seed(4)
-
-def relu(x):
-    return np.clip(x, 0, None)
-
-def dsigmoid(y):
-    return 1.0 - y * y
-
-
-def dsoftmax(x):
-    return np.clip(np.sign(x), 0, 1)
-
-
-def stable_softmax(x):
-    """Compute softmax values for each sets of scores in x."""
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=0)
-
-def assert_no_nans(struct):
-    '''Make sure there are no np.nan floating around in the array/dict'''
-    if isinstance(struct, dict):
-        ret_val = np.isnan(struct.values()).any()
-    else:
-        ret_val = np.isnan(struct).any()
-    if ret_val:
-        raise AssertionError(struct)
 
 
 class NN(object):
